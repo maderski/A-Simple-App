dcfe8b1b26c487c8f221396009e14639656c36a6
Jason Maderski <maderski@gmail.com>
2023-05-30 11:28:04 -0400

    moved logic to decide which state to the viewModel

--- app/src/main/java/maderski/asimpleapp/userdirectory/presentation/userlocation/UserMapLocationScreen.kt
+++ app/src/main/java/maderski/asimpleapp/userdirectory/presentation/userlocation/UserMapLocationScreen.kt
@@ -2,9 +2,12 @@ package maderski.asimpleapp.userdirectory.presentation.userlocation
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -15,34 +18,45 @@ import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import maderski.asimpleapp.common.content.ErrorMessageContent
+import maderski.asimpleapp.common.content.LoadingContent
+import maderski.asimpleapp.userdirectory.presentation.userlocation.UserMapLocationViewModel.*
 
 @Composable
 fun UserMapLocationScreen(
     companyName: String?,
     locationLat: String?,
     locationLng: String?,
+    viewModel: UserMapLocationViewModel = hiltViewModel()
 ) {
-    val lat = locationLat?.toDoubleOrNull()
-    val lng = locationLng?.toDoubleOrNull()
-    if (lat != null && lng != null) {
-        val companyLocation = LatLng(lat, lng)
-        val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(companyLocation, 5f)
-        }
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
-        ) {
-            Marker(
-                position = companyLocation,
-                title = companyName,
-                icon = BitmapDescriptorFactory.defaultMarker(
-                    BitmapDescriptorFactory.HUE_RED
+    val screenState by viewModel.screenState.collectAsState()
+    HandleScreenState(state = screenState)
+
+    viewModel.init(companyName, locationLat, locationLng)
+}
+
+@Composable
+private fun HandleScreenState(state: UIState) {
+    when (state) {
+        UIState.Loading -> LoadingContent()
+        is UIState.ShowingMap -> {
+            val companyLocation = state.latLng
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(companyLocation, 5f)
+            }
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPositionState
+            ) {
+                Marker(
+                    position = companyLocation,
+                    title = state.companyName,
+                    icon = BitmapDescriptorFactory.defaultMarker(
+                        BitmapDescriptorFactory.HUE_RED
+                    )
                 )
-            )
+            }
         }
-    } else {
-        ErrorMessageContent(message = "Lat and/or Lng cannot be Null! Lat: $lat Lng: $lng")
+        is UIState.ErrorOccurred -> ErrorMessageContent(message = state.message)
     }
 }
 
--- app/src/main/java/maderski/asimpleapp/userdirectory/presentation/userlocation/UserMapLocationViewModel.kt
+++ app/src/main/java/maderski/asimpleapp/userdirectory/presentation/userlocation/UserMapLocationViewModel.kt
@@ -1,17 +1,42 @@
 package maderski.asimpleapp.userdirectory.presentation.userlocation
 
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class UserMapLocationViewModel @Inject constructor() : ViewModel() {
     sealed interface UIState {
         object Loading : UIState
-        data class ShowingLocationOnMap(
-            val latitude: String,
-            val longitude: String,
+        data class ShowingMap(
+            val companyName: String,
+            val latLng: LatLng,
         ) : UIState
+
         data class ErrorOccurred(val message: String) : UIState
+    }
+
+    val screenState: StateFlow<UIState>
+        get() = _screenState
+    private val _screenState = MutableStateFlow<UIState>(UIState.Loading)
+
+    fun init(
+        companyName: String?,
+        locationLat: String?,
+        locationLng: String?,
+    ) {
+        val lat = locationLat?.toDoubleOrNull()
+        val lng = locationLng?.toDoubleOrNull()
+        val name = companyName ?: "Unknown"
+        if (lat != null && lng != null) {
+            _screenState.value = UIState.ShowingMap(name, LatLng(lat, lng))
+        } else {
+            _screenState.value = UIState.ErrorOccurred(
+                "Lat and/or Lng cannot be Null! Lat: $lat Lng: $lng"
+            )
+        }
     }
 }
