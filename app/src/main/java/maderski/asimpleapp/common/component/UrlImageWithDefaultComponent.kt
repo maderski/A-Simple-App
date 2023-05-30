91e87bbdd011b69e4109057f1582efbc1e649039
Jason Maderski <maderski@gmail.com>
2023-05-29 23:02:29 -0400

    create the UrlImageWithDefaultComponent

---
+++ app/src/main/java/maderski/asimpleapp/common/component/UrlImageWithDefaultComponent.kt
@@ -0,0 +1,46 @@
+package maderski.asimpleapp.common.component
+
+import android.graphics.BitmapFactory
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import maderski.asimpleapp.R
+
+@Composable
+fun UrlImageWithDefaultComponent(
+    modifier: Modifier = Modifier,
+    imageUrl: String?,
+    @DrawableRes defaultImage: Int,
+) {
+    if (imageUrl != null) {
+        AsyncImage(
+            modifier = modifier,
+            model = imageUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+    } else {
+        val context = LocalContext.current
+        Image(
+            modifier = modifier,
+            bitmap = BitmapFactory.decodeResource(
+                context.resources,
+                defaultImage
+            ).asImageBitmap(),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewUrlImageWithDefaultComponentShowingDefault() {
+    UrlImageWithDefaultComponent(imageUrl = null, defaultImage = R.drawable.user_placeholder)
+}
